de36317361eda2741235a48602038ff14aa2e929
Jannie Louwrens <jannie-louwrens@users.noreply.github.com>
2022-02-01 23:20:35 +0200

    Ng 13 upgrade (#23)
    * ng upgrade to v9
    
    * ng upgrade to v10
    
    * ng upgrade to v11
    
    * ng upgrade to v12
    
    * ng upgrade to v13
    
    * upgrade dependencies
    
    * fix code after angular version upgrade
    
    * fix css after bootstrap upgrade

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,42 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { DatePipe } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { CollapseModule, TooltipModule, ModalModule, BsDropdownModule } from 'ngx-bootstrap';
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
-import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
-import { environment } from 'src/environments/environment';
+import { KeycloakService, KeycloakAngularModule } from "keycloak-angular";
+import { environment } from "src/environments/environment";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TwoDigitDecimalNumberDirective } from './directives/two-digit-decimal-number.directive';
-import { HttpErrorInterceptor} from './interceptor/http-error.interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TwoDigitDecimalNumberDirective } from "./directives/two-digit-decimal-number.directive";
+import { HttpErrorInterceptor } from "./interceptor/http-error.interceptor";
 
-import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
-import { ProductComponent } from './components/product/product.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { OrdersComponent } from './components/orders/orders.component';
-import { CustomerOrdersComponent } from './components/customer-orders/customer-orders.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CustomerStore } from './stores/customer.store';
-import { AlertComponent } from './components/alert/alert.component';
+import { ProductCatalogComponent } from "./components/product-catalog/product-catalog.component";
+import { ProductComponent } from "./components/product/product.component";
+import { CustomersComponent } from "./components/customers/customers.component";
+import { OrdersComponent } from "./components/orders/orders.component";
+import { CustomerOrdersComponent } from "./components/customer-orders/customer-orders.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { CustomerStore } from "./stores/customer.store";
+import { AlertComponent } from "./components/alert/alert.component";
 
-export function kcInitializer(keycloak: KeycloakService): () => Promise<any> {
-  return (): Promise<any> => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await keycloak.init(environment.keycloakOptions);
-        console.log('Keycloak is initialized');
-        resolve();
-      } catch (error) {
-        console.log('Error thrown in init ' + error);
-        reject(error);
-      }
-    });
-  };
+export function kcInitializer(
+  keycloak: KeycloakService
+): () => Promise<boolean> {
+  return () => keycloak.init(environment.keycloakOptions);
 }
 
 @NgModule({
@@ -49,7 +43,7 @@ export function kcInitializer(keycloak: KeycloakService): () => Promise<any> {
     TwoDigitDecimalNumberDirective,
     CustomerOrdersComponent,
     CartComponent,
-    AlertComponent
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
@@ -62,15 +56,19 @@ export function kcInitializer(keycloak: KeycloakService): () => Promise<any> {
     CollapseModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
   ],
   providers: [
     DatePipe,
     CustomerStore,
-    { provide: APP_INITIALIZER, useFactory: kcInitializer, multi: true, deps: [KeycloakService] },
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+    {
+      provide: APP_INITIALIZER,
+      useFactory: kcInitializer,
+      multi: true,
+      deps: [KeycloakService],
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,29 +1,30 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { KeycloakConfig, KeycloakInitOptions, KeycloakOptions } from 'keycloak-angular';
+import { KeycloakOptions } from "keycloak-angular";
+import { KeycloakConfig, KeycloakInitOptions } from "keycloak-js";
 
 // Add here your keycloak configuration information
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8080/auth',
-  realm: 'demo',
-  clientId: 'my-app'
+  url: "http://localhost:8080/auth",
+  realm: "demo",
+  clientId: "my-app",
 };
 
 const keycloakInitOptions: KeycloakInitOptions = {
-  onLoad: 'login-required',
-  checkLoginIframe: false
+  onLoad: "login-required",
+  checkLoginIframe: false,
 };
 
 const keycloakOptions: KeycloakOptions = {
   config: keycloakConfig,
   initOptions: keycloakInitOptions,
-  enableBearerInterceptor: true
+  enableBearerInterceptor: true,
 };
 
 export const environment = {
   production: false,
-  keycloakOptions
+  keycloakOptions,
 };
 
 /*
@@ -33,4 +34,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
