3299fadefd4648ae2c64db9459051cf04babdbc4
Jannie Louwrens <jannie@opencollab.co.za>
2022-02-01 23:17:25 +0200

    fix css after bootstrap upgrade

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,32 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { CustomerInfo } from './models/customer.info';
-import { CustomerStore } from './stores/customer.store';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { CustomerInfo } from "./models/customer.info";
+import { CustomerStore } from "./stores/customer.store";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: [`
-    .count {
-      padding: 2px 3px;
-      z-index:15;
-      position:relative;
-      left: -10px;
-      top:-10px
-    }
-  `]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      .count {
+        padding: 2px 3px;
+        z-index: 15;
+        position: relative;
+        left: -5px;
+        top: -12px;
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
-
   isCollapsed = true;
   customer$: Observable<CustomerInfo>;
 
-  constructor(
-    private router: Router,
-    private customerStore: CustomerStore) {
-      this.customerStore.init();
-    }
+  constructor(private router: Router, private customerStore: CustomerStore) {
+    this.customerStore.init();
+  }
 
   ngOnInit() {
     this.customer$ = this.customerStore.getAll$();
@@ -36,10 +35,8 @@ export class AppComponent implements OnInit {
     this.customerStore.login();
   }
 
-
   async doLogout() {
-    await this.router.navigate(['/']);
+    await this.router.navigate(["/"]);
     await this.customerStore.logout();
   }
-
 }
