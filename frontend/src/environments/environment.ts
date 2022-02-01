26083868d12d89363b9608ed187ea026ba4da2a9
Jannie Louwrens <jannie@opencollab.co.za>
2022-02-01 22:25:23 +0200

    fix code after angular version upgrade

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,29 +1,30 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { KeycloakConfig, KeycloakInitOptions, KeycloakOptions } from 'keycloak-angular';
+import { KeycloakOptions } from "keycloak-angular";
+import { KeycloakConfig, KeycloakInitOptions } from "keycloak-js";
 
 // Add here your keycloak configuration information
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8080/auth',
-  realm: 'demo',
-  clientId: 'my-app'
+  url: "http://localhost:8080/auth",
+  realm: "demo",
+  clientId: "my-app",
 };
 
 const keycloakInitOptions: KeycloakInitOptions = {
-  onLoad: 'login-required',
-  checkLoginIframe: false
+  onLoad: "login-required",
+  checkLoginIframe: false,
 };
 
 const keycloakOptions: KeycloakOptions = {
   config: keycloakConfig,
   initOptions: keycloakInitOptions,
-  enableBearerInterceptor: true
+  enableBearerInterceptor: true,
 };
 
 export const environment = {
   production: false,
-  keycloakOptions
+  keycloakOptions,
 };
 
 /*
