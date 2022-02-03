49039168580e92dd1eaa4d0e504bcb4a8afd4f8d
Jannie Louwrens <jannie@opencollab.co.za>
2022-02-03 23:00:02 +0200

    feature: add clarity design system

---
+++ frontend/src/app/shared/modules/my-clarity-design/my-clarity-design.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { ClarityModule } from "@clr/angular";
+
+@NgModule({
+  declarations: [],
+  imports: [ClarityModule],
+  exports: [ClarityModule],
+})
+export class MyClarityDesignModule {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MyNgxBootstrapModule } from "./modules/my-ngx-bootstrap/my-ngx-bootstrap.module";
+import { MyClarityDesignModule } from "./modules/my-clarity-design/my-clarity-design.module";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MyNgxBootstrapModule],
-  exports: [MyNgxBootstrapModule],
+  imports: [CommonModule, MyNgxBootstrapModule, MyClarityDesignModule],
+  exports: [MyNgxBootstrapModule, MyClarityDesignModule],
 })
 export class SharedModule {}
